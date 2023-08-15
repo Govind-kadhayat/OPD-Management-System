48571f58bdc8cd86ba62e046fdf470e0bf7208b8
Govind.K <131758703+Govind-kadhayat@users.noreply.github.com>
2023-08-15 17:56:41 +0545

    Add files via upload

--- OPD/opd.sql
+++ OPD/opd.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 17, 2023 at 12:05 PM
--- Server version: 10.4.27-MariaDB
--- PHP Version: 8.2.0
+-- Generation Time: Aug 15, 2023 at 02:09 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -34,7 +34,8 @@ CREATE TABLE `bill` (
   `address` varchar(200) NOT NULL,
   `phno` bigint(50) NOT NULL,
   `consultant` varchar(200) NOT NULL,
-  `user` varchar(200) NOT NULL,
+  `user` int(200) NOT NULL,
+  `user_id` int(200) NOT NULL,
   `dob` datetime(6) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -82,7 +83,9 @@ CREATE TABLE `particular` (
 INSERT INTO `particular` (`id`, `invoice_no`, `paticular`, `qty`, `price`) VALUES
 (5, NULL, '0', 1, 1200),
 (5, NULL, '0', 12, 12000),
-(5, NULL, 'ECO', 12, 12000);
+(5, NULL, 'ECO', 12, 12000),
+(1, NULL, 'Seroflow', 12, 2500),
+(1, NULL, 'Seroflow', 12, 3500);
 
 -- --------------------------------------------------------
 
@@ -92,8 +95,10 @@ INSERT INTO `particular` (`id`, `invoice_no`, `paticular`, `qty`, `price`) VALUE
 
 CREATE TABLE `patient_list` (
   `id` int(200) NOT NULL,
+  `user_id` int(100) NOT NULL,
   `name` varchar(200) NOT NULL,
   `Doctor` varchar(200) NOT NULL,
+  `dr_id` int(200) NOT NULL,
   `phno` bigint(200) NOT NULL,
   `Department` varchar(200) NOT NULL,
   `user` varchar(200) NOT NULL,
@@ -106,13 +111,12 @@ CREATE TABLE `patient_list` (
 -- Dumping data for table `patient_list`
 --
 
-INSERT INTO `patient_list` (`id`, `name`, `Doctor`, `phno`, `Department`, `user`, `address`, `Status`, `dob`) VALUES
-(1, 'Rahul', 'Dr.surendra', 0, 'Cardio', '', 'kalopool', '', NULL),
-(2, 'Sagar Chaudhary', 'Ganesh', 0, 'Cardio', '', 'Dhangadhi', '', NULL),
-(5, 'RAMESH', ' Surendra', 32423423423, ' General Physican', 'surendra', 'mahendranger', 'pending', '2023-07-12 00:00:00.000000'),
-(6, 'RAMESH', ' Surendra', 32423423423, ' General Physican', 'surendra', 'mahendranger', 'pending', '2023-07-12 00:00:00.000000'),
-(7, 'Asmita Bhattta', 'Ganesh', 0, 'Cardio', '', 'Lalipur', '', NULL),
-(8, 'Sandy', ' Surendra', 989898998, ' General Physican', 'Govind', 'Maitidevi', 'pending', '2023-07-26 00:00:00.000000');
+INSERT INTO `patient_list` (`id`, `user_id`, `name`, `Doctor`, `dr_id`, `phno`, `Department`, `user`, `address`, `Status`, `dob`) VALUES
+(1, 2, 'Rahul', 'Dr.surendra', 2, 0, ' General Physican', '   Surendra ', '   kalopool', 'Accepted', NULL),
+(2, 3, 'Sagar Chaudhary', 'Ganesh', 3, 0, 'Cardio', '', 'Dhangadhi', '', NULL),
+(6, 5, 'RAMESH', ' Surendra', 2, 32423423423, ' General Physican', '  surendra', '  mahendranger', 'Pending', '2023-07-12 00:00:00.000000'),
+(7, 6, 'Asmita Bhattta', 'Ganesh', 3, 0, ' General Physican', ' Surendra', ' Lalipur', 'Accepted', NULL),
+(8, 7, 'Sandy', ' Surendra', 2, 989898998, ' General Physican', 'Govind', 'Maitidevi', 'pending', '2023-07-26 00:00:00.000000');
 
 -- --------------------------------------------------------
 
@@ -126,15 +130,49 @@ CREATE TABLE `register` (
   `Email` varchar(200) NOT NULL,
   `Password` varchar(200) NOT NULL,
   `MNumber` bigint(50) NOT NULL,
-  `Address` varchar(200) NOT NULL
+  `Address` varchar(200) NOT NULL,
+  `user_type` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `register`
 --
 
-INSERT INTO `register` (`id`, `Name`, `Email`, `Password`, `MNumber`, `Address`) VALUES
-(2, 'Govind', 'Govind@gmail.com', '$2y$10$JSpqSA5sSbRPrfC8ql3mN.fs.f0JB2nBhTzoT3IMHH2ALTQ7cw4qe', 9868585585, 'PatanDhoka,lalitpur');
+INSERT INTO `register` (`id`, `Name`, `Email`, `Password`, `MNumber`, `Address`, `user_type`) VALUES
+(2, 'Govind', 'Govind@gmail.com', '$2y$10$JSpqSA5sSbRPrfC8ql3mN.fs.f0JB2nBhTzoT3IMHH2ALTQ7cw4qe', 9868585585, 'PatanDhoka,lalitpur', 'Admin\r\n'),
+(3, 'Super Admin', 'Superadmin@gmail.com', '$2y$10$gobes/CQeq6myfI5TucMVukIhynS3dX.fQIgAmxKw89VOPzpqtvQS', 9868585585, 'Patandhoka', ''),
+(4, 'Akash', 'akash12@gmail.com', '$2y$10$TmfrviSBFF587XzgTg2Oae0s1ig2IDJeG1R5iSUlE8Odef4aHcTm2', 9856858585, 'Dhading', ''),
+(5, 'Akash', 'Ak12@gmail.com', '$2y$10$ofbwQi6N5pDHYyC3ddEece6gY367QUQjLlYuDU7LmtmPH7KVSNiay', 9856858585, 'Dhading', 'Admin'),
+(6, 'Local', 'local12@gmail.com', '$2y$10$yeWkIx5saqMfOIQ1oAxFgeuuLSyqmnRTpy3l/5FPu7zf7m1Sq9Tme', 9898989898, 'Kathmand', 'User'),
+(7, 'new', 'new@gmail.com', '$2y$10$HXWdb.L7YN6yYJPPlqpIN.BbCjH7QqHYkM7h/Y8d8bWxXOBubm.tq', 9898989898, 'ktm', 'Admin'),
+(9, 'Kabir', 'kabir@gmail.com', '$2y$10$xsXRD9WT5s/5oS43cQspPu1obADLwyjZ3jCCI/Fb2Q2mO2OSd457K', 9898989898, 'swyambu', 'User'),
+(10, 'Admin', 'Admin@gmail.com', '$2y$10$NEJFaSUxaDxhp1IbJPc4puHCE76PkVz4FpVj.oLEeFTurMuAzwigm', 9868585585, 'Patan dhoka', ' Admin'),
+(11, 'User', 'User@gmail.com', '$2y$10$6S8eM4IEWpFUpsErXVnP5eTaN8ZtQ8f1EvuD6DCaURl1ivY3AakN2', 9868585585, 'chabil', 'User');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `test`
+--
+
+CREATE TABLE `test` (
+  `id` int(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `type` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `test`
+--
+
+INSERT INTO `test` (`id`, `email`, `password`, `type`) VALUES
+(1, 'Govind@gmail.com', 'Govind@123', 'Admin\r\n'),
+(2, 'Kadhayat@gmail.com', 'Kadhayat@123', 'User\r\n'),
+(3, 'Admin@gmail.com', 'Admin@123', 'Admin'),
+(4, 'govind@gmail.com', 'govind@123', 'Admin'),
+(5, 'u@gmail.com', 'u@123', 'User\r\n'),
+(6, 'User@gmail.com', 'User@123', 'User');
 
 --
 -- Indexes for dumped tables
@@ -144,7 +182,8 @@ INSERT INTO `register` (`id`, `Name`, `Email`, `Password`, `MNumber`, `Address`)
 -- Indexes for table `bill`
 --
 ALTER TABLE `bill`
-  ADD PRIMARY KEY (`invoce_no`);
+  ADD PRIMARY KEY (`invoce_no`),
+  ADD KEY `u_id` (`user_id`);
 
 --
 -- Indexes for table `doctor_detail`
@@ -162,12 +201,20 @@ ALTER TABLE `particular`
 -- Indexes for table `patient_list`
 --
 ALTER TABLE `patient_list`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `dr_id` (`dr_id`);
 
 --
 -- Indexes for table `register`
 --
 ALTER TABLE `register`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `test`
+--
+ALTER TABLE `test`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -196,17 +243,30 @@ ALTER TABLE `patient_list`
 -- AUTO_INCREMENT for table `register`
 --
 ALTER TABLE `register`
-  MODIFY `id` int(200) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(200) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- Constraints for dumped tables
 --
 
 --
+-- Constraints for table `bill`
+--
+ALTER TABLE `bill`
+  ADD CONSTRAINT `u_id` FOREIGN KEY (`user_id`) REFERENCES `register` (`id`);
+
+--
 -- Constraints for table `particular`
 --
 ALTER TABLE `particular`
   ADD CONSTRAINT `f_key` FOREIGN KEY (`invoice_no`) REFERENCES `bill` (`invoce_no`);
+
+--
+-- Constraints for table `patient_list`
+--
+ALTER TABLE `patient_list`
+  ADD CONSTRAINT `dr_id` FOREIGN KEY (`dr_id`) REFERENCES `doctor_detail` (`id`),
+  ADD CONSTRAINT `user_id` FOREIGN KEY (`user_id`) REFERENCES `register` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
